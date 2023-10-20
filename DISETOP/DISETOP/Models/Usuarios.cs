f11b633069bde66e1520079870bc0c0605d9d8ad
ivan9218 <ivanprogra1@gmail.com>
2023-10-20 01:06:38 -0600

    SE AGREGA EL PROYECTO

---
+++ DISETOP/DISETOP/Models/Usuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PRUEBAS_LOGIN.Models
+{
+    public class Usuarios
+    {
+        public int id_Usuario { get; set; }
+        public string Usuario { get; set; }
+        public string Contrasena { get; set; }
+
+
+        public string ConfirmarClave { get; set; }
+
+    }
+}
